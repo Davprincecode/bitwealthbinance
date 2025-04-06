0137d4e96ac2c6fff6464f3605556248aeed0737
david <obafemidavprince@gmail.com>
2025-04-06 01:17:31 +0100

    testneth

--- app/Services/BinanceFuturesService.php
+++ app/Services/BinanceFuturesService.php
@@ -23,10 +23,10 @@ class BinanceFuturesService
         // $this->apiKey = 'v83VOrFC6b1yq3tbdoGRMj7l0bYyGnb589z6MhA6L3z4nM9ejJGTCO2sfHhYK7qD';
         // $this->apiSecret = '0jThJwNQBIu35cJtWAHulsTWrpD9PunGdAEi53nvBIxqd746eojx9EanBex1OFgO';
         // $this->baseUrl = 'fapi.binance.com';
-
-        $this->apiKey = '1c9c2e1837b75f91ee5bbd0b3d15ef5571868946644c8a0da5ed2e8d461fbb1c';
+// fbb1c
+        $this->apiKey = '1c9c2e1837b75f91ee5bbd0b3d15ef5571868946644c8a0da5ed2e8d461';
         $this->apiSecret = '38dc061581dfb14a693b61eaa4d637e1de0c98129635f2c672b5f993a6ec3d2b';
-        
+
         $this->baseUrl = "https://testnet.binancefuture.com";
     }
 
