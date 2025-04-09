e3ed56b457b9182cf9af2bb1ef76d44c1cdbf0a2
david <obafemidavprince@gmail.com>
2025-04-09 16:45:56 +0100

    api_key

--- app/Http/Middleware/VerifyApiCredentials.php
+++ app/Http/Middleware/VerifyApiCredentials.php
@@ -15,15 +15,17 @@ class VerifyApiCredentials
      */
     public function handle(Request $request, Closure $next): Response
     {
+        dd($request->headers->all());
+        
         $apiKey = $request->header('api_key');
         $secretKey = $request->header('secret_key');
 
-        // if (!$apiKey || !$secretKey) {
-        //     return response()->json([
-        //         'status' => false,
-        //         'message' => 'API key and Secret key are required.'
-        //     ], 401);
-        // }
+        if (!$apiKey || !$secretKey) {
+            return response()->json([
+                'status' => false,
+                'message' => 'API key and Secret key are required.'
+            ], 401);
+        }
 
         $request->merge(['api_key' => $apiKey, "secret_key" => $secretKey]);
         return $next($request);
